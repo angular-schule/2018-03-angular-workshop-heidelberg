5fcfff312c62ce1ceb39bc6494b22a6dbd28d431
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-03-06 11:15:04 +0100

    hello typescript

---
+++ typescript-playground/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var test_1 = require("./test");
+var test = new test_1.Test(2);
+console.log("ende");
+//# sourceMappingURL=main.js.map
