0c7b9db167b15cfd31a49118ea563def20c54f0d
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-03-06 14:18:13 +0100

    Dashboard mit string[]

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'br';
+  title = 'Welt';
+
+  constructor() {
+    setTimeout(() => this.title = 'Heidelberg', 2000);
+  }
 }
