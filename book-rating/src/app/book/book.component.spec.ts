2600e87b1b8a3e15c2ad001b1dd38dd92ceebb2c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-03-07 17:45:52 +0100

    testing button click on native element

--- book-rating/src/app/book/book.component.spec.ts
+++ book-rating/src/app/book/book.component.spec.ts
@@ -2,6 +2,7 @@ import { Book } from './../shared/book';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { By } from '@angular/platform-browser';
 
 describe('BookComponent', () => {
   let component: BookComponent;
@@ -17,9 +18,6 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
-  });
-
-  it('should call book.rateUp on component.rateUp', () => {
 
     component.book = {
       title: 'Test',
@@ -30,9 +28,16 @@ describe('BookComponent', () => {
 
     spyOn(component.book, 'rateUp');
     fixture.detectChanges();
+  });
 
+  it('should call book.rateUp on component.rateUp', () => {
     component.rateUp();
     expect(component.book.rateUp).toHaveBeenCalled();
+  });
 
+  it('should call book.rateUp on button click', () => {
+    const myButton = fixture.debugElement.query(By.css('[test-rateUpButton]'));
+    myButton.nativeElement.click();
+    expect(component.book.rateUp).toHaveBeenCalled();
   });
 });
