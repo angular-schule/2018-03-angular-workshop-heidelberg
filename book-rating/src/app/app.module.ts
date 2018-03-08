711f40c2aee26be13b8c67c814512c67ca3acd88
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-03-08 16:47:13 +0100

    routing

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { BookStoreService } from './shared/book-store.service';
 import { HttpClientModule } from '@angular/common/http';
 import { RxjsPlaygroundComponent } from './rxjs-playground/rxjs-playground.component';
 import { CreateBookComponent } from './create-book/create-book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { CreateBookComponent } from './create-book/create-book.component';
     DashboardComponent,
     BookComponent,
     RxjsPlaygroundComponent,
-    CreateBookComponent
+    CreateBookComponent,
+    BookDetailsComponent
   ],
   imports: [   // Module werden importiert!
     BrowserModule,
